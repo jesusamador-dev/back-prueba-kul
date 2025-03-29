60dcd03d72d2fe4e6bfcf7888c6fe354bbab53a9
Jesus Macias <jesus.macias.amador@gmail.com>
2025-03-28 22:55:09 -0600

    fix: se cambia el samesite

--- src/presentation/routers/keys_routes.py
+++ src/presentation/routers/keys_routes.py
@@ -42,9 +42,8 @@ def get_rsa_keys(user_id: str,
     response.set_cookie(
         key="user-keys",
         value=user_id,
-        httponly=False,
-        samesite="none",
-        max_age=60 * 60 * 24,
+        httponly=True,
+        samesite="Lax",
         secure=True
     )
     return {
